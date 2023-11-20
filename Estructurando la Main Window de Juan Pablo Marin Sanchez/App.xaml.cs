e34db5199a18f40402baa0ea4f100403cf9f6871
jmarinsanc <jmarinsanc@educacion.navarra.es>
2023-11-20 15:22:19 +0100

    añadido estructurando main window para coger código

---
+++ Estructurando la Main Window de Juan Pablo Marin Sanchez/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Estructurando_la_Main_Window_de_Juan_Pablo_Marin_Sanchez
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
