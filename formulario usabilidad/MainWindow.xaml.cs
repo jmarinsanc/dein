58436621554835acaee80698a0a9dcc7d5348012
Juan Pablo Marin <jmarinsanc@educacion.navarra.es>
2024-03-03 17:49:06 +0100

    formulario usabilidad hecho

---
+++ formulario usabilidad/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace formulario_de_juan_pablo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string email { get; set; }
+        public string telefono { get; set; }
+        public string dni { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SeleccionarImagen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de Imagen|*.jpg;*.jpeg;*.png;*.bmp|Todos los archivos|*.*";
+            progressBar.Value = progressBar.Value+10;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string rutaImagen = openFileDialog.FileName;
+
+                // Cargar la imagen seleccionada en el control Image
+                BitmapImage imagenBitmap = new BitmapImage(new Uri(rutaImagen));
+                imagenPrevia.Source = imagenBitmap;
+            }
+        }
+
+        private void boton_cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            primera nuevaVentana = new primera();
+            nuevaVentana.Show();
+            nuevaVentana.boxnombre.Text = this.nombre;
+            nuevaVentana.box_apellidos.Text = this.apellidos;
+            nuevaVentana.box_email.Text = this.email;
+            nuevaVentana.box_telefono.Text = this.telefono;
+            nuevaVentana.box_dni.Text = this.dni;
+            this.Close();
+        }
+
+        private void boton_guardar_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Empleado nuevoEmpleado = new Empleado(nombre, apellidos, email, telefono);
+            datagrid.Items.Add(nuevoEmpleado);
+            progressBar.Value = 100;
+
+
+        }
+
+
+
+
+
+
+
+        public class Empleado
+        {
+            public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public string email { get; set; }
+            public string telefono { get; set; }
+
+            public Empleado(string nombre, string apellidos, string email, string telefono)
+            {
+                this.nombre = nombre;
+                this.apellidos = apellidos;
+                this.email = email;
+                this.telefono = telefono;
+            }
+        }
+
+        private void gotFocus(object sender, RoutedEventArgs e)
+        {
+            if(sender is TextBox textbox)
+            {
+                if(!String.IsNullOrWhiteSpace(textbox.Text))
+                {
+                    textbox.Text = "";
+                }
+                
+            }
+        }
+
+        private void Txt_lostFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox textbox)
+            {
+                if (String.IsNullOrWhiteSpace(textbox.Text))
+                {
+                    if (textbox.Name == "box_direccion")
+                    {
+                        textbox.Text="Dirección";
+                    }else if(textbox.Name == "box_ciudad"){
+                        textbox.Text = "Ciudad";
+                    }
+                    else if(textbox.Name == "box_provincia")
+                    textbox.Text = "Provincia";
+                    else if (textbox.Name == "box_codigo")
+                        textbox.Text = "Código Postal";
+                    else if (textbox.Name == "box_pais")
+                        textbox.Text = "País";
+                }
+            }
+        }
+
+        private void box_pais_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (box_pais.Text != "")
+            {
+                progressBar.Value = progressBar.Value + 10;
+            }
+
+        }
+
+        private void box_redsocial_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (box_redsocial.Text != "")
+            {
+                progressBar.Value = progressBar.Value + 10;
+            }
+
+                
+
+        }
+
+        private void textBox_puesto_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (textBox_puesto.Text !="")
+            {
+                progressBar.Value = progressBar.Value + 10;
+            }
+        }
+    }
+}
---
+++ formulario usabilidad/primera.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace formulario_de_juan_pablo
+{
+    /// <summary>
+    /// Lógica de interacción para primera.xaml
+    /// </summary>
+    public partial class primera : Window
+    {
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string email { get; set; }
+        public string telefono { get; set; }
+        public string dni { get; set; }
+
+        public primera()
+        {
+            InitializeComponent();
+            nombre = boxnombre.Text;
+            apellidos = box_apellidos.Text;
+            email = box_email.Text;
+            telefono = box_telefono.Text;
+            dni = box_dni.Text;
+        }
+
+        private void Button_Cancelar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Guardar(object sender, RoutedEventArgs e)
+        {
+            if (boxnombre.Text == "")
+            {
+                MessageBox.Show("El campo Nombre no puede estar vacío", "Error");
+            }
+            else if (box_apellidos.Text == "")
+            {
+                MessageBox.Show("El campo Apellidos no puede estar vacío", "Error");
+            }
+            else if (!Regex.IsMatch(box_email.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("El campo E-mail no puede estar vacío o formato incorrecto", "Error");
+            }
+            else if (!Regex.IsMatch(box_telefono.Text, @"^\d{9}$"))
+            {
+                MessageBox.Show("El campo Teléfono no puede estar vacío o debe tener 9 dígitos", "Error");
+            }
+            else
+            {
+                MainWindow main = new MainWindow();
+                main.nombre = boxnombre.Text;
+                main.apellidos = box_apellidos.Text;
+                main.email = box_email.Text;
+                main.telefono = box_telefono.Text;
+                main.Show();
+                this.Close();
+            }
+
+   
+        }
+
+        private void box_dni_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if ( boxnombre.Text != "" && box_email.Text != "" && box_apellidos.Text != "" && box_telefono.Text != "")
+            {
+                progressBar.Value = 60;
+
+            }
+
+
+        }
+
+        private void boxnombre_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (boxnombre.Text == "" && progressBar.Value == 0)
+            {
+                progressBar.Value = progressBar.Value + 10;
+
+            }
+
+        }
+
+        private void box_apellidos_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (progressBar.Value == 10 && boxnombre.Text !="")
+            {
+                progressBar.Value = progressBar.Value + 10;
+
+            }
+
+        }
+
+        private void box_email_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if ( boxnombre.Text != "" && box_apellidos.Text!="")
+            {
+                progressBar.Value = 30;
+
+            }
+
+        }
+
+        private void box_telefono_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (box_telefono.Text == "" && boxnombre.Text != "" && box_email.Text !="" && box_apellidos.Text != "")
+            {
+                progressBar.Value = 40;
+
+            }
+
+        }
+
+        private void fecha_nacimiento_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (fecha_nacimiento.Text == "" && progressBar.Value ==40 && boxnombre.Text != "" && box_email.Text != "" && box_apellidos.Text != "" &&box_telefono.Text!="")
+            {
+                progressBar.Value = 50;
+
+            }
+
+        }
+    }
+}
